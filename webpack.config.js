956836dc0f02acb884c1dfc747302497367a3897
Megh Parikh <meghprkh@gmail.com>
2015-12-09 13:14:19 +0530

    Add build system

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,47 @@
 var path = require('path');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
+const TARGET = process.env.npm_lifecycle_event;
 
-module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/example'
-  ],
+var common = {
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'lib'),
     filename: 'bundle.js',
     publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
   }
 };
+
+if (TARGET === 'start') {
+  module.exports = merge(common, {
+    devtool: 'eval',
+    entry: [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './src/example'
+    ],
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      }]
+    }
+  })
+} else {
+  module.exports = merge(common, {
+    entry: ['./src/App'],
+    output: {
+      filename: 'index.js'
+    },
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      }]
+    }
+  });
+}
